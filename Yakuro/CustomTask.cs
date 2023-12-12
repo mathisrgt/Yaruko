d8300a6d934bc6773faa75b0756c2175a048349c
Johan LIU <johan.liu@edu.devinci.fr>
2023-12-12 17:04:40 +0100

    Revert "updating project to ASP.NET"
    This reverts commit 5a9b211ffa030fbdfef3713af82d4db73f495a50.

---
+++ Yakuro/CustomTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CustomTask
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Priority { get; set; }
+    public List<string> Tags { get; set; }
+    public DateTime Deadline { get; set; }
+
+    public CustomTask(string title, string description, int priority)
+    {
+        Title = title;
+        Description = description;
+        Priority = priority;
+        Tags = new List<string>();
+        Deadline = DateTime.MinValue; // Initialisation avec une valeur par défaut
+    }
+}
