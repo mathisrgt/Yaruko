993a3db0a05f8043cde9c1db07045cbbbe04cbb7
Mathis <sergent.mathis@gmail.com>
2023-12-12 15:39:09 +0100

    first upload
    add first class : customtask

---
+++ Yakuro/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        CustomTask nouvelleTache = CustomTask.RemplirTask();
+        Console.WriteLine("Tâche créée :");
+        Console.WriteLine(nouvelleTache.ToString());
+        Console.ReadKey();
+    }
+}
