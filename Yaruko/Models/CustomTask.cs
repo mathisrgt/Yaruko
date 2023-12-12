5a9b211ffa030fbdfef3713af82d4db73f495a50
Mathis <sergent.mathis@gmail.com>
2023-12-12 16:57:18 +0100

    updating project to ASP.NET
    updating project to ASP.NET

---
+++ Yaruko/Models/CustomTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yaruko.Models
+{
+    public class CustomTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+        public List<string> Tags { get; set; }
+        public DateTime Deadline { get; set; }
+    }
+}
---
+++ Yaruko/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Yaruko
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Task}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
